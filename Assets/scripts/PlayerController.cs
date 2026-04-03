d3ac6ac48484b31fe934529d6ede17893c35f2f2
Dheerendra Verma <dheeruvermrpr@gmail.com>
2026-04-04 05:10:07 +0530

    candy catch game

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public bool canMove = true;
-    [SerializeField]float moveSpeed;
+    [SerializeField] float moveSpeed;
     [SerializeField] float maxPosXOfPlayer;
 
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if (canMove)
@@ -19,48 +19,14 @@ public class PlayerController : MonoBehaviour
             Move();
         }
     }
-    private void Move()
+    public void Move()
     {
-        float inputX = 0f;
-
-        // Desktop (keyboard/controller)
-        inputX = Input.GetAxis("Horizontal");
-
-        // Mobile (touch)
-        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
-
-                // Example: move based on touch position relative to screen center
-                if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
-                {
-                    if (touch.position.x < Screen.width / 2)
-                    {
-                        inputX = -1f; // move left
-                    }
-                    else
-                    {
-                        inputX = 1f; // move right
-                    }
-                }
-            }
-        }
-
-        // Apply movement
+        float inputX = Input.GetAxis("Horizontal");
         transform.position += Vector3.right * inputX * moveSpeed * Time.deltaTime;
-
-        // Clamp position
         float xPos = Mathf.Clamp(transform.position.x, -maxPosXOfPlayer, maxPosXOfPlayer);
         transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
     }
 
-    //private void Move()
-    //{
-    //    float intpuX= Input.GetAxis("Horizontal");
-    //    transform.position += Vector3.right * intpuX *moveSpeed*Time.deltaTime;
-    //    float xPos=Mathf.Clamp(transform.position.x, -maxPosXOfPlayer, maxPosXOfPlayer);
-    //    transform.position=new Vector3(xPos,transform.position.y,transform.position.z);
-    //}
 }
+
+
